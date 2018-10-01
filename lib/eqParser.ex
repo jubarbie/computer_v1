eabeff765f7a99ebf8c5fce00af7afbba3de3e34
Jules BARBIER <jubarbie@e3r4p8.42.fr>
2018-10-01 16:35:47 +0200

    more

--- lib/eqParser.ex
+++ lib/eqParser.ex
@@ -1,8 +1,20 @@
 defmodule EqParser do
 
   def parse(str) do
-    a = 1
-    b = 2
+	tab = String.split str, "="
+	case tab do
+		[left, "0"] ->	extractABC left
+		_ -> fn() 	->	IO.puts "Wrong equation format. Ex: a * X^2 + b * X + c = 0"
+			 			System.halt(0)
+		 				end
+	end
+	left = tab
     {:ok, b}
   end
+
+  def extractABC str do
+	  list = String.split str, "+"
+	  Enum.group_by(list, [a: 0, b: 0, c: 0], fn elem -> elem )
+  end
+
 end
