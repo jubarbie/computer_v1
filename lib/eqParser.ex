59f3fdb35192101a8fcfcfde98ac391b067eff98
Jules BARBIER <jubarbie@e2r2p12.42.fr>
2018-12-07 16:44:59 +0100

    parsing ok

--- lib/eqParser.ex
+++ lib/eqParser.ex
@@ -1,20 +1,22 @@
 defmodule EqParser do
 
-  def parse(str) do
-	tab = String.split str, "="
-	case tab do
-		[left, "0"] ->	extractABC left
-		_ -> fn() 	->	IO.puts "Wrong equation format. Ex: a * X^2 + b * X + c = 0"
-			 			System.halt(0)
-		 				end
+  def parseABC(str) do
+	eq = String.replace(str, ~r/ +/, "")
+	regex = ~r/^(?<a>[+,-]?\d*)\*?X\^?2((?:<b>[+,-]\d*)\*?X(\^?1)?)?((?:<c>[+,-]\d+)(\*?X\^?0)?)?$/i
+	case Regex.named_captures(regex, eq, [capture: :all_names]) |> parseABCtoInt do
+		[{a, ""}, {b, ""}, {c, ""}] -> {:ok, %{a: a, b: b, c: c}}
+		b -> b
+		_ -> {:error, "Malformed equation"}
 	end
-	left = tab
-    {:ok, b}
   end
 
-  def extractABC str do
-	  list = String.split str, "+"
-	  Enum.group_by(list, [a: 0, b: 0, c: 0], fn elem -> elem )
+  def parseABCtoInt(%{"a" => a, _}) do
+  	Enum.map [a, b, c], fn e -> Integer.parse e end
+  end
+
+  def parseABCtoInt(_) do
+	IO.puts "Malformed equation"
+	System.halt(0)
   end
 
 end
--- test/eq_parser_test.exs
+++ test/eq_parser_test.exs
@@ -1,7 +1,11 @@
 defmodule EqParserTest do
   use ExUnit.Case
 
-  test "3 * X^2 + 2 * X + 10 = 0" do
-    assert EqParser.parse("3 * X^2 + 2 * X + 10 = 0") == {:ok, {:deg2, [a: 3, b: 2, c: 10]}}
+  test "Equation parsing test" do
+    assert EqParser.parseABC("3 * X^2 + 2 * X + 10") == {:ok, %{a: 3, b: 2, c: 10}}
+	assert EqParser.parseABC("3*X^2+2*X+10") == {:ok, %{a: 3, b: 2, c: 10}}
+	assert EqParser.parseABC("3*X^2+2*X-10") == {:ok, %{a: 3, b: 2, c: -10}}
+	assert EqParser.parseABC("X^2+2*X-10") == {:ok, %{a: 3, b: 2, c: -10}}
+	assert EqParser.parseABC("2*X-10") == {:error, "Malformed equation"}
   end
 end
